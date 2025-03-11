be07241e632b10dd745ef95b96ea1dd3587d0771
aguero-tech <developer@aguero.tech>
2025-03-11 14:02:37 -0500

    testing component.spec.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+
+// Custom components
 import { LogoComponent } from './component/logo/logo.component';
 import { MatrixComponent } from './component/matrix/matrix.component';
 import { SidenavButtonsComponent } from './component/sidenav-buttons/sidenav-buttons.component';
 import { TopHeaderComponent } from './component/top-header/top-header.component';
 import { ActivityDescriptionComponent } from './component/activity-description/activity-description.component';
-import { ymlService } from './service/yaml-parser/yaml-parser.service';
-import { HttpClientModule } from '@angular/common/http';
 import { CircularHeatmapComponent } from './component/circular-heatmap/circular-heatmap.component';
 import { MappingComponent } from './component/mapping/mapping.component';
 import { ReadmeToHtmlComponent } from './component/readme-to-html/readme-to-html.component';
@@ -25,6 +27,9 @@ import { AboutUsComponent } from './component/about-us/about-us.component';
 import { DependencyGraphComponent } from './component/dependency-graph/dependency-graph.component';
 import { TeamsComponent } from './component/teams/teams.component';
 import { ToStringValuePipe } from './pipe/to-string-value.pipe';
+
+// Services
+import { ymlService } from './service/yaml-parser/yaml-parser.service';
 
 @NgModule({
   declarations: [
@@ -46,22 +51,19 @@ import { ToStringValuePipe } from './pipe/to-string-value.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
     ReactiveFormsModule,
+    MatFormFieldModule, // Added
+    MatInputModule, // Added
+    MatChipsModule, // Added
+    MatExpansionModule, // Added
+    MatTableModule, // Added
+    AppRoutingModule,
+    MaterialModule,
     HttpClientModule,
-    MatExpansionModule, // Imported here
-    MatFormFieldModule, // Imported here
-    MatInputModule, // Imported here
-  ],
-  exports: [
-    MatExpansionModule, // Exported for other modules
-    MatFormFieldModule, // Exported for other modules
-    MatInputModule, // Exported for other modules
   ],
   providers: [ymlService],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Ensures Angular recognizes custom elements
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Avoids unknown element issues
 })
 export class AppModule {}
