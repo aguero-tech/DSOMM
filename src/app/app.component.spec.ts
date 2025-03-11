edeb32c16fd3f222334251bbe375321c24e604e9
aguero-tech <developer@aguero.tech>
2025-03-11 13:54:06 -0500

    testing component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,14 +2,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 describe('AppComponent', () => {
   let app: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTableModule,
+        MatChipsModule,
+        MatExpansionModule,
+      ],
       declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA], // ✅ Prevents unknown element errors
     }).compileComponents();
   });
 
@@ -32,7 +48,6 @@ describe('AppComponent', () => {
     const HTMLElement: HTMLElement = fixture.nativeElement;
     var divTag = HTMLElement.querySelector('div')!;
     var aTag = divTag.querySelector('a')!;
-    //console.log(aTag);
     expect(aTag.textContent).toEqual('Fork Aguero GitHub');
   });
 });
